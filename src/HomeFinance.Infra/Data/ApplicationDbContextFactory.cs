1eddf61c3d190e31a72e32f16a8cc5a5fd20032b
Jean <jeanmaster777@gmail.com>
2022-11-15 11:45:54 -0300

    configurado efcore gerado scaffolding

---
+++ src/HomeFinance.Infra/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeFinance.Infra.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseNpgsql("User ID=postgres;Password=71321787;Host=localhost;Port=5432;Database=HomeFinance;Pooling=true;");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
--- src/HomeFinance.Models/Models/Finances.cs
+++ src/HomeFinance.Models/Models/Finances.cs
@@ -1,10 +1,13 @@
-﻿namespace HomeFinance.Domain.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HomeFinance.Domain.Models
 {
     public class Finances
     {
+        [Column("id_finances")]
         public Guid FinancesId { get; set; }
         public string FinanceName { get; set; }
-        public int DueDate { get; set; }
+        public DateTime DueDate { get; set; }
         public decimal Price { get; set; }
     }
 }
