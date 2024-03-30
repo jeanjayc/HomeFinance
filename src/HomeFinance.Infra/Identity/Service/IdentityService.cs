79acc7aab54e637537d037917eb8b427d6ab4212
jean Carlos <jeanmaster777@gmail.com>
2024-03-30 12:19:28 -0300

    add primeira parte identity

---
+++ src/HomeFinance.Infra/Identity/Service/IdentityService.cs
@@ -0,0 +1,71 @@
+﻿using HomeFinance.Infra.DTOs.Request;
+using HomeFinance.Infra.DTOs.Response;
+using HomeFinance.Infra.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
+namespace HomeFinance.Infra.Identity.Service
+{
+    public class IdentityService : IIdentityService
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public IdentityService(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+        public async Task<UsuarioCadastroResponse> CadastrarUsuario(UsuarioCadastroRequest usuarioCadastro)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = usuarioCadastro.Email,
+                Email = usuarioCadastro.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(identityUser, usuarioCadastro.Senha);
+            if(result.Succeeded)
+            {
+                await _userManager.SetLockoutEnabledAsync(identityUser, false);
+            }
+
+            var usuarioCadastroResponse = new UsuarioCadastroResponse(result.Succeeded);
+            if(!result.Succeeded && result.Errors.Any())
+            {
+                usuarioCadastroResponse.AdicionarErro(result.Errors.Select(e => e.Description));
+            }
+
+            return usuarioCadastroResponse;
+        }
+
+        public async Task<UsuarioLoginResponse> LoginUsuario(UsuarioLoginRequest usuarioLogin)
+        {
+            var result = await _signInManager.PasswordSignInAsync(usuarioLogin.Email, usuarioLogin.Senha, false, true);
+
+            var usuarioLoginResponse = new UsuarioLoginResponse(result.Succeeded);
+
+            if (!result.Succeeded)
+            {
+                if(result.IsLockedOut)
+                {
+                    usuarioLoginResponse.Erros.Add("Usuário bloqueado por tentativas inválidas");
+                }
+                else if(result.IsNotAllowed)
+                {
+                    usuarioLoginResponse.Erros.Add("Usuário não está autorizado a acessar o sistema");
+                }
+                else if(result.RequiresTwoFactor)
+                {
+                    usuarioLoginResponse.Erros.Add("Usuário ou senha inválidos");
+                }
+                else
+                {
+                    usuarioLoginResponse.Erros.Add("Usuário ou senha inválidos");
+                }
+            }
+
+            return usuarioLoginResponse;
+        }
+    }
+}
